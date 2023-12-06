2d19a801dad917e360531879473e3f488083b50a
Muhammad Iqbal Gobel <muhammadiqbal.g@yahoo.com>
2023-12-06 07:56:50 +0700

    feat : profile page

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,5 +7,17 @@ use Illuminate\Http\Request;
 class ProfileController extends Controller
 {
     //
-    
+    public function index() {
+        $nama = 'Muhammad Iqbal Gobel';
+        $program_mbkm = 'Student Independen';
+        $program_studi = 'Fullstack Web Developer';
+        $mitra = 'GITS Indonesia';
+
+        $data['nama'] = $nama;
+        $data['program_mbkm'] = $program_mbkm;
+        $data['program_studi'] = $program_studi;
+        $data['mitra'] = $mitra;
+
+        return view('profile/index', $data);
+    }
 }
